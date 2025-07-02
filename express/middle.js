a42777f654261ff4459912b1637b736a6032cda4
Aaditya-5281 <aadityakumar26082004@gmail.com>
2025-07-02 13:11:11 +0530

    This is Middleware

---
+++ express/middle.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+const app = express()
+
+
+function http (req,res,next){
+    const header=req.headers
+    console.log(req.headers);
+    console.log(req.url);
+    next()
+}
+
+app.use(http)
+
+app.get('/https',function(req,res){
+    res.send(req.statusCode)
+})
+
+app.listen(3000)
